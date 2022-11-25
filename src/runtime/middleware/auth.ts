88eeea21914f0ce36d4112188e53325b0177638f
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-11-24 23:22:53 -0300

    feat: add `auth` middleware (#2)
    * feat: add `auth` middleware
    
    * feat: remove `onUnauthenticated` from `useSession`
    
    Use `auth` middleware instead

---
+++ src/runtime/middleware/auth.ts
@@ -0,0 +1,32 @@
+import { defineNuxtRouteMiddleware, navigateTo, useState } from '#app'
+import { withQuery } from 'ufo'
+import type { RouteLocationNormalized } from 'vue-router'
+import { useSession } from '../composables/session'
+import { joinPathToBase } from '../utils'
+
+export default defineNuxtRouteMiddleware((to) => {
+  if (to.meta.auth === false) {
+    return
+  }
+
+  const pageIsInGuestMode = to.meta.auth === 'guest'
+  const { status } = useSession()
+
+  if (status.value === 'unauthenticated' && !pageIsInGuestMode) {
+    // Update loading state
+    const loading = useState<boolean>('auth:loading')
+    loading.value = true
+
+    // Redirect to signIn
+    const url = withQuery(joinPathToBase('signin'), {
+      error: 'SessionRequired',
+      callbackUrl: to.path
+    })
+    return navigateTo(url, { external: true })
+  }
+
+  if (status.value === 'authenticated' && pageIsInGuestMode) {
+    // Redirect to home
+    return navigateTo('/')
+  }
+})
